ff6b59733e20759a97e10d3c4c47ade8a64117e2
Itsodepth <fuyukiraiko@gmail.com>
2025-03-17 22:39:20 +0700

    update link sosmed dan url_launcher

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key, required String title});
+
+  // Fungsi untuk membuka tautan URL
+  void _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Tidak dapat membuka $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,49 +25,68 @@ class ProfilePage extends StatelessWidget {
         backgroundColor: Colors.blue[900],
       ),
       body: Center(
-          child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 30), // Spasi dari atas
-            // Gambar profil
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: AssetImage(
-                  'assets/images/me.jpg'), // Ganti dengan path gambar Anda
-            ),
-            SizedBox(height: 30), // Spasi antara gambar dan teks
-            // Nama pengguna
-            Text(
-              'Muh Devano Alfarizy',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 30),
+              // Gambar profil
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('assets/images/me.jpg'),
               ),
-            ),
-            SizedBox(height: 10), // Spasi antara nama dan deskripsi
-            // Deskripsi singkat
-            Text(
-              'Mahasiswa semester 4 di UDB',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey[600],
+              SizedBox(height: 30),
+              // Nama pengguna
+              Text(
+                'Muh Devano Alfarizy',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 30), // Spasi antara deskripsi dan kontak
-            Text(
+              SizedBox(height: 10),
+              // Deskripsi singkat
+              Text(
+                'Mahasiswa semester 4 di UDB',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.grey[600],
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 30),
+              // Deskripsi panjang
+              Text(
                 'Perkenalkan nama saya Muh Devano Alfarizy. Saya lahir di kota Surakarta pada tanggal 3 Maret 2004. Saat ini saya tinggal di Kartasura. Saya saat ini sedang menempuh pendidikan di Universitas Duta Bangsa. Semoga saya bisa mencapai cita-cita untuk menjadi seorang programmer yang handal.',
                 style: TextStyle(
                   fontSize: 16,
                   color: Colors.grey[600],
                 ),
-                textAlign: TextAlign.justify),
-          ],
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(height: 30),
+              // Informasi Kontak
+              ListTile(
+                leading: Icon(Icons.phone, color: Colors.blue[900]),
+                title: Text('089674577831'),
+                onTap: () => _launchURL('tel:089674577831'),
+              ),
+              ListTile(
+                leading: Icon(Icons.email, color: Colors.blue[900]),
+                title: Text('devanoalfarizy04@gmail.com'),
+                onTap: () => _launchURL('mailto:devanoalfarizy04@gmail.com'),
+              ),
+              ListTile(
+                leading: Icon(Icons.camera_alt, color: Colors.blue[900]),
+                title: Text('@itsodepth'),
+                onTap: () => _launchURL('https://www.instagram.com/itsodepth'),
+              ),
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
